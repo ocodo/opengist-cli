27539a6130221aacbc7a944c409bc1e12d2d26dc
jasonm23 <jasonm23@gmail.com>
2026-09-11 16:34:18 +0700

    make base .

--- pages/vite.config.ts
+++ pages/vite.config.ts
@@ -5,6 +5,7 @@ import path from "node:path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "./",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
