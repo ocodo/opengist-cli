f650a9d282d6b341fd5a63319462539bedbd71b6
Jason Milkins <jasonm23@gmail.com>
2026-09-12 15:56:16 +0700

    Clean up: move font to public folder, update paths to use /OcodoMonoDotZeroNerdFont-Light.woff2

--- pages/src/main.tsx
+++ pages/src/main.tsx
@@ -1,20 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import fontUrl from "./assets/OcodoMonoDotZeroNerdFont-Light.woff2?url";
 import { App } from "./App";
 import "./index.css";
-
-// Inject font-face with guaranteed asset path
-const style = document.createElement("style");
-style.textContent = `
-  @font-face {
-    src: url("${fontUrl}");
-    font-family: "OMDZero";
-    font-weight: 300;
-    font-style: normal;
-  }
-`;
-document.head.appendChild(style);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
