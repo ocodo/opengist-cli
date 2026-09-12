fdc661243547caee5a03ae4154cf1872cd45be59
jasonm23 <jasonm23@gmail.com>
2026-09-12 13:00:18 +0700

    add GithubIcon component and recolor/reposition download icon

--- pages/src/App.tsx
+++ pages/src/App.tsx
@@ -3,23 +3,31 @@ import readme from "../README.md?raw";
 import rehypeHighlight from "rehype-highlight";
 import "@highlightjs/cdn-assets/styles/github-dark.min.css";
 import { CloudDownload } from "lucide-react";
+import { GithubIcon } from "@/components/GHIcon";
 
 export const App = () => (
   <main className="min-h-screen bg-background/70 text-foreground">
     <article className="prose prose-neutral dark:prose-invert mx-auto max-w-3xl px-6 py-12 markdown">
-      <div className="flex flex-row gap-5 items-center justify-center pb-10">
-        <div className="text-5xl font-black tracking-tighter m-0 p-0">
+
+      <a href="https://github.com/ocodo/opengist-cli" target='_blank'>
+        <svg xmlns="http://www.w3.org/2000/svg" className="fixed top-0 right-0 w-30 h-30" viewBox="0 0 100 100">
+          <polygon points="0,0 100,0 100,100" fill="#00000040" />
+        </svg>
+        <GithubIcon className="m-2.5 fixed top-0 right-0" />
+      </a>
+      <a href="opengist-cli" download="opengist-cli">
+        <CloudDownload className="fixed top-4 left-4 stroke-[1px] w-8 h-8 bg-foreground text-background rounded-full p-1.5 cursor-pointer" />
+      </a>
+
+      <div className="flex flex-col items-center text-5xl font-black tracking-tighter py-6">
+        <div>
           Opengist CLI
         </div>
-
-        <a href="opengist-cli" download="opengist-cli">
-          <CloudDownload className="stroke-[1px] w-11 h-11 bg-cyan-800 rounded-full p-1.5 cursor-pointer" />
-        </a>
       </div>
 
-      <div className="flex flex-row gap-2 text-sm justify-center font-light">
+      <div className="flex flex-wrap gap-2 text-sm justify-center font-light">
         Download <a href="opengist-cli" download="opengist-cli" className="decoration-0 hover:decoration-1">opengist-cli</a>
-        requires <a href="https://github.com/astral-sh/uv" target="_blank" className="decoration-0 hover:decoration-1">uv,</a> run on Linux or MacOS
+        requires <a href="https://github.com/astral-sh/uv" target="_blank" className="decoration-0 hover:decoration-1">uv,</a> runs on all platforms uv does
       </div>
 
       <Markdown rehypePlugins={[rehypeHighlight]}>
